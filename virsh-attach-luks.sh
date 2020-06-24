dc5a639fecad563f9027acc0cc1ecee7f54977d5
elasmo <elasmo@github>
2020-06-24 13:47:07 +0200

    Find next available target device, also fixed some typos.

--- virsh-attach-luks.sh
+++ virsh-attach-luks.sh
@@ -32,12 +32,13 @@ done
 
 trap error ERR
 
-domain_name="$1"
+domain="$1"
 size="10G"
 vol_dir="$(virsh pool-dumpxml default | xmllint --xpath '//path/text()' -)"
-cryptvol_dir="$vol_path/cryptovols"
-cryptvol_name="$cryptvol_dir/$domain_name-$(date +%y%m%d).luks"
-cryptvol_mapper="$domain_name-$(date +%y%m%d).mapper"
+cryptvol_dir="$vol_path/cryptvols"
+cryptvol_path="$cryptvol_dir/$domain-$(date +%y%m%d).luks"
+cryptvol_name="$(basename $cryptvol_path)"
+cryptvol_mapper="$domain-$(date +%y%m%d).mapper"
 passphrase="$(openssl rand -hex 32)"
 
 # Check if domain exists
@@ -45,16 +46,27 @@ if ! virsh list --all --name | grep "$domain" >/dev/null; then
     error "$domain: Not found"
 fi
 
+# Check if cryptvol already exist
+[ -f "$cryptvol_path" ] && error "$cryptvol_name: Already exist"
+
 # Find next free device target to use
-targets="$(virsh dumpxml meeting | xmllint --xpath '//domain/devices/disk/target[@bus="virtio"]/@dev' -)"
+targets="$(virsh dumpxml $domain | xmllint --xpath '//domain/devices/disk/target[@bus="virtio"]/@dev' -)"
+
+can_use=1
 for t in b c d e f; do
     for target in $targets; do
-        target="$(echo $target | cut -f2 -d'=' | tr -dc a-z)"
-        if [ "$target" != "vd$t" ]; then
-            vol_target="vd$t"
-            break 2
-        fi
+        device="$(echo $target | cut -f2 -d'=' | tr -dc a-z)"
+
+        # Check if disk ident is listed among existent device targets
+        [ "$device" = "vd$t" ] && can_use=0
     done
+
+    if [ $can_use -eq 1 ]; then
+        vol_target="vd$t"
+        break
+    fi
+
+    can_use=1
 done
 
 # Set volume size if specified
@@ -63,19 +75,19 @@ done
 # Create volume directory
 [ ! -d "${vol_root}" ] && mkdir -p "$cryptvol_dir"
 
-echo "[*] Truncating $cryptvol_name to $size"
-truncate -s "$size" "$cryptvol_name"
+echo "[*] Truncating \"$cryptvol_name\" to $size"
+truncate -s "$size" "$cryptvol_path"
 
 echo "[*] Initializes a LUKS partition"
-echo "$passphrase" | sudo cryptsetup luksFormat "$cryptvol_name"
+echo "$passphrase" | sudo cryptsetup luksFormat "$cryptvol_path"
 echo "[*] Passphrase: $passphrase"
 
 echo "[*] Creating ext4 filesystem"
-echo "$passphrase" | sudo cryptsetup open "$cryptvol_name" "$cryptvol_mapper"
+echo "$passphrase" | sudo cryptsetup open "$cryptvol_path" "$cryptvol_mapper"
 sudo mkfs.ext4 "/dev/mapper/$cryptvol_mapper" > /dev/null 2>&1
 sync
 sudo cryptsetup close "$cryptvol_mapper"
 unset $passphrase
 
-echo "[*] Attaching disk to $domain_name"
-virsh attach-disk "$domain_name" "$cryptvol_name" "$vol_target" --cache none > /dev/null
+echo "[*] Attaching $cryptvol_name to $domain"
+virsh attach-disk "$domain" "$cryptvol_path" "$vol_target" --cache none > /dev/null
