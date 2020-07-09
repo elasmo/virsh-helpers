ed9b84d688d25c7918f2ade4d690e14637dfff22
elasmo <elasmo@github>
2020-07-09 16:34:01 +0200

    Added getopts

--- virsh-share.sh
+++ virsh-share.sh
@@ -12,11 +12,9 @@ error () {
 }
 
 usage () {
-    echo "Usage: $SCRIPT_NAME <source> <dest>" 1>&2
+    echo "Usage: $SCRIPT_NAME [-f source] [-t dest]" 1>&2
     exit 1
 }
-
-[ $# -ne 2 ] && usage
 
 # Check dependencies
 for dep in virsh xmllint; do
@@ -24,5 +22,28 @@ for dep in virsh xmllint; do
         error "$dep: Not found"
     fi
 done
+
+# Parse arguments
+[ $# -lt 1 ] && usage
+
+while getopts f:t:h opt
+do      
+    case "$opt" in
+        f) src_domain="$OPTARG";;
+        t) dst_domain="$OPTARG";;
+        ?) usage;;
+        esac
+done
+
+# Share between geust and host
+if [ -n "$src_domain" ] && [ -z "$dst_domain" ]; then
+    echo 
+# Share between guest and guest
+elif [ -n "$src_domain" ] && [ -n "$dst_domain" ]; then
+    echo
+# Share between host and guest
+elif [ -z "$src_domain" ] && [ -n "$dst_domain" ]; then
+    echo
+fi
 
 echo "dummy"
